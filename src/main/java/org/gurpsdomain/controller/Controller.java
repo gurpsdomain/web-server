fb0726be45326b01bb229d6bf60d92a8a2c8e0f2
Marcel Gorissen <damnhighc4@hotmail.com>
2017-03-29 14:36:25 +0200

    Added simple POST method for returning consumed json

--- src/main/java/org/gurpsdomain/controller/Controller.java
+++ src/main/java/org/gurpsdomain/controller/Controller.java
@@ -2,10 +2,12 @@ package org.gurpsdomain.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
 
 import static org.springframework.http.ResponseEntity.status;
 
@@ -13,7 +15,7 @@ import static org.springframework.http.ResponseEntity.status;
 public class Controller {
 
     @RequestMapping(method = RequestMethod.GET, value = "/{sheet:.+\\.json}")
-    public ResponseEntity returnSheet() {
+    public ResponseEntity returnSheetText() {
         if ("marcel.json".equals("marcel.json")) {
 
             String sheetBody;
@@ -22,5 +24,23 @@ public class Controller {
         } else {
             return status(HttpStatus.NOT_FOUND).build();
         }
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/convert", produces = {"application/json"}, consumes = "application/json")
+    @ResponseBody
+    public String convert(@RequestBody String inputJson) {
+
+//        Reader reader = new StringReader(inputJson);
+//        Writer writer = new StringWriter();
+//
+//        SheetInput input = fromYaml(reader);
+//        SheetOutput output = toJson(writer);
+//
+//        Pipeline.flow(input).into(output);
+//
+//        String outputJson = writer.toString();
+//
+//        return outputJson;
+        return inputJson;
     }
 }
